a5bf466f871d273470d2b62eec3dd7d6f97233b3
bulbajs <artval.kuptsov@gmail.com>
2025-10-08 12:24:06 +0300

    #6.3.1 Editted
    - Чуть подправил текст после долгого перерыва

--- simpleapp/apps.py
+++ simpleapp/apps.py
@@ -2,8 +2,9 @@ from django.apps import AppConfig
 
 
 class SimpleappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'simpleapp'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         from . import signals
+        
